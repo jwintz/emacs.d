0cf1224ba1bbe2c034454c1782563efc9e4e9e63
Julien Wintz <julien.wintz@inria.fr>
2026-01-07 13:09:18 +0100

    Embark integration

--- modules/init-completion.el
+++ modules/init-completion.el
@@ -41,6 +41,23 @@
   :config
   (setq consult-preview-key nil))
 
+(use-package embark
+  :ensure t
+  :bind (("C-." . embark-act)
+         ("C-;" . embark-dwim)
+         ("C-h B" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect\\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
 (provide 'init-completion)
 
 ;;; init-completion.el ends here
