98492b622ccd9aef124ac90f29266edb82314ec3
Julien Wintz <julien.wintz@inria.fr>
2026-01-09 21:22:21 +0100

    Stick to eshell. Configure aliases.

--- init/init-terminal.el
+++ init/init-terminal.el
@@ -5,23 +5,18 @@
 (use-package eshell
   :bind ("C-c s s" . eshell)
   :init
-  (setq eshell-save-history-on-exit t)
-  (setq eshell-hist-ignoredups t)
-  ;; eshell-scroll-to-bottom-on-input 'all
   ;; eshell-error-if-no-glob t
   ;; eshell-prefer-lisp-functions nil
-  ;; eshell-destroy-buffer-when-process-dies t
+  (setq eshell-save-history-on-exit t)
+  (setq eshell-hist-ignoredups t)
+  (setq eshell-scroll-to-bottom-on-input 'all)
+  (setq eshell-destroy-buffer-when-process-dies t)
   (setq eshell-login-script (expand-file-name "conf/eshlogin" emacs-config-dir))
   (setq eshell-rc-script (expand-file-name "conf/eshrc" emacs-config-dir))
+  (setq eshell-aliases-file (expand-file-name "conf/eshaliases" emacs-config-dir))
   :config
   (with-eval-after-load 'esh-mode
     (bind-key "C-c s k" #'iota-shell-clear eshell-mode-map))
-
-  ;; Ensure em-term is loaded and add bat to visual commands
-  (require 'em-term)
-
-  (add-to-list 'eshell-visual-commands "bat")
-  (add-to-list 'eshell-visual-commands "more")
 
   (use-package capf-autosuggest
     :hook (eshell-mode . capf-autosuggest-mode)
@@ -39,16 +34,6 @@
 
       (setq capf-autosuggest-capf-functions
             '(hyalo-capf-autosuggest-history-capf)))))
-
-(use-package eat
-  :ensure t
-  :hook (eshell-load-hook . eat-eshell-mode)
-  :config
-  ;; Robustly ensure Eat integration is active in every Eshell buffer
-  (defun init-terminal--setup-eat ()
-    (eat-eshell-mode)
-    (eat-eshell-visual-command-mode))
-  (add-hook 'eshell-mode-hook #'init-terminal--setup-eat))
 
 (use-package iota-shell
    :ensure nil
