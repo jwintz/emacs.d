232dc0c1f98df25e6e555a2a91ab1c9de58820b6
Julien Wintz <julien.wintz@inria.fr>
2026-01-12 12:56:38 +0100

    login script fixed.

--- init/init-modes.el
+++ init/init-modes.el
@@ -3,8 +3,12 @@
 ;;; Code:
 
 ;; TOML mode
-
 (use-package toml-mode)
+
+;; Eshell configuration files
+(add-to-list 'auto-mode-alist '("/conf/eshlogin\'" . emacs-lisp-mode))
+(add-to-list 'auto-mode-alist '("/conf/eshrc\'" . emacs-lisp-mode))
+(add-to-list 'auto-mode-alist '("/conf/eshaliases\'" . eshell-mode))
 
 (provide 'init-modes)
 
